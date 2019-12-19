645649e243f2516153d9edcd625a3572131dbddc
BotPixel <46143060+BotPixel@users.noreply.github.com>
2019-12-18 19:39:11 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+<!-- Original Template by Justin Raniel Pascual -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PixelQuery</title>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,400" rel="stylesheet">
+		<link href="https://elitesnaps.co.uk/plugins/aos.css" rel="stylesheet">
+		<meta property="og:description" content="Get free gems, coins & coupons!">
+	</head>
+	<body>
+		
+		<nav>
+			<h1>PixelQuery</h1>
+		</nav>
+		
+		<div class="container in">
+			<div class="center">
+				<img data-aos="fade-down" src="test.jpg" width="200px" height="200px"><br>
+			</div>
+			<h1 data-aos="zoom-out" style="color:white">PixelQuery</h1>
+			<h2 data-aos="zoom-in">Free gems, coins, & coupons</h2>
+		</div>
+		
+		<div data-aos="fade-up" class="container-1">
+			<h2>About PixelQuery...</h2><br><hr width="40px"><br>
+		   <p> Recieve <b>free gems</b>, <b>coins</b>, and <b>coupons</b>!<br><br>
+			   You can earn <b>2 billion</b> for each currency, a lifetime supply!<br>
+			   Just a <b>one-time</b> activation and you are set for the future of Pixel Gun 3D!<br><br>
+			 <h1>Our job is to satisfy the community with what they really want and deserve.</h1>
+			</p>
+		</div>
+		
+				<div data-aos="fade-up" class="container-1">
+			<h2>Why are we hacking currency? </h2><br><hr width="40px"><br>
+		   <p> - We want to give users what they <b>truly deserve and want</b>.<br>
+		   - Pixel Gun 3D has <b>committed illicit behavior</b>, those that are just morally wrong.<br>
+		   - The game is <b>pay to win</b> and it is <b>too difficult</b> to gather up currency nowadays.<br>
+		   <br>
+		   <h1>Most importantly, we want to <b>start a petition</b> to lower prices of purchases and to <b>reduce in-app purchases.</b></h1>
+			</p>
+		</div>
+
+		
+		<div data-aos="fade-down" class="container-2">
+			<h1>SKILLS</h1><br><hr width="40px"><br>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+			</p><br>
+			
+			<p>Hacking</p><br>
+			<div class="container">
+			  <div class="skills html">90%</div>
+			</div>
+			<br>
+			<p>Cooperation</p><br>
+			<div class="container">
+			  <div class="skills css">90%</div>
+			</div>
+			<br>
+			<p>Shitting</p><br>
+			<div class="container">
+			  <div class="skills js">20%</div>
+			</div>
+			<br>
+			<p>Eating</p><br>
+			<div class="container">
+			  <div class="skills php">80%</div>
+			</div>
+		</div>
+		
+		<footer>
+			<p>More features coming soon!</p>
+		</footer>
+		<script src="https://elitesnaps.co.uk/plugins/aos.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script type="text/javascript">
+			AOS.init({
+	  		duration: 2000,
+			});
+		</script>
+
+	</body>
+</html>
